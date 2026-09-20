6647c2ad09aa371e67379b07b3df72d3cb684168
alireza <al7rezakln@gmail.com>
2026-09-20 07:00:43 +0330

    second commit on second training of day16.\nAdded a func (predicate2) it works like predicate func but it's using comprehensions

--- day16_T2.py
+++ day16_T2.py
@@ -1,7 +1,30 @@
 l1 = ["ali", "python", "REZA", "", "   ", "programming"]
 
 def predicate(strings: list[str]) -> list[str]:
-    l1 = list(map(lambda s: s.capitalize(), filter(lambda s: s.strip()!="", strings)))
-    return l1
+    """
+    Reads a list of strings ,fliters the empty ones and capitalize the remaining ones using python funcs.
+    Personaly prefer this to predicate 2 because it is more profesional than doing it with comprehension.
+    Parameters:
+        strings (str): Reads a list of random strings that may be some empty strings among them.
+    Returns:
+        list[str]: A list wich is a filtered and capitalized version of input that has no empty sttrings. 
+    """
+    l_01 = list(map(lambda s: s.capitalize(), filter(lambda s: s.strip()!="", strings)))
+    return l_01
 
-print(predicate(l1))
+l2 = predicate(l1)
+
+def predicate2(strings: list[str]) -> list[str]:
+    """
+        Reads a list of strings ,fliters the empty ones and capitalize the remaining ones using comprehensions.
+        
+        Parameters:
+            strings (str): Reads a list of random strings that may be some empty strings among them.
+        Returns:
+            list[str]: A list wich is a filtered and capitalized version of input that has no empty sttrings. 
+        """
+    l_01 = [string.strip() for string in strings if string.strip()!=""]
+    l_02 = [string.capitalize() for string in l_01]
+    return l_02
+
+print(predicate2(l1))
